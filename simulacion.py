e7d114eb7bf9e057db5fdcd193e37cf9f9c60990
Marthox1999 <mateo.gregory@correounivalle.edu.co>
2019-08-10 19:19:32 -0500

    Primeras pruebas de simulaciones ademas se almacenan los tiempos de espera de cada vehiculo

--- simulacion.py
+++ simulacion.py
@@ -1,3 +1,6 @@
+import numpy as np
+import math
+
 tiempoGlobal = 0
 
 tiempoSemaforosRojo = 55
@@ -8,15 +11,39 @@ tiempoSemaforo1 = [tiempoSemaforo1Verde, True]
 tiempoSemaforo2 = [tiempoSemaforo2Verde, False]
 tiempoLiberarFlujo = [tiempoSemaforosRojo, False]
 
+tiempoLlegadaCola1 = math.ceil(np.random.exponential(scale=9, size=1))
+tiempoLlegadaCola2 = math.ceil(np.random.exponential(scale=12, size=1))
+
 ciclo = 2
 
-retrasoArranque = 2
+retrasoArranque1 = 2
+retrasoArranque2 = 2
 
-cola1 = [] 
+cola1 = []
 cola2 = []
 
-while tiempoGlobal < 200 :
+esperaCola1 = []
+esperaCola2 = []
+
+while tiempoGlobal < 10000 :
+	cola1 = [x + 1 for x in cola1]
+	cola2 = [x + 1 for x in cola2]
 	tiempoGlobal += 1
+	print(cola1, " - ", cola2)
+
+	if(tiempoLlegadaCola1 != 0):
+		tiempoLlegadaCola1 -= 1
+	else:
+		tiempoLlegadaCola1 = math.ceil(np.random.exponential(scale=9, size=1))
+		if(tiempoSemaforo1[1] == False):
+			cola1.append(1)
+
+	if(tiempoLlegadaCola2 != 0):
+		tiempoLlegadaCola2 -= 1
+	else:
+		tiempoLlegadaCola2 = math.ceil(np.random.exponential(scale=12, size=1))
+		if(tiempoSemaforo2[1] == False):
+			cola2.append(1)
 
 	if(tiempoSemaforo1[1]):
 		tiempoSemaforo1[0] -= 1
@@ -25,6 +52,17 @@ while tiempoGlobal < 200 :
 			tiempoSemaforo1 = [tiempoSemaforo1Verde, False]
 			tiempoSemaforo2 = [tiempoSemaforo2Verde, False]
 			tiempoLiberarFlujo = [tiempoSemaforosRojo, True]
+			retrasoArranque1 = 2
+		else:
+			if(retrasoArranque1 != 0):
+				retrasoArranque1 -= 1
+			else:
+				try:
+					tiempoEsperando = cola1.pop(0)
+					esperaCola2.append(tiempoEsperando)
+				except:
+					0
+				retrasoArranque1 = 2
 
 	if(tiempoSemaforo2[1]):
 		print("2 Semaforo Verde")
@@ -33,6 +71,17 @@ while tiempoGlobal < 200 :
 			tiempoSemaforo2 = [tiempoSemaforo2Verde, False]
 			tiempoSemaforo1 = [tiempoSemaforo1Verde, False]
 			tiempoLiberarFlujo = [tiempoSemaforosRojo, True]
+			retrasoArranque2 = 2
+		else:
+			if(retrasoArranque2 != 0):
+				retrasoArranque2 -= 1
+			else:
+				try:
+					tiempoEsperando = cola2.pop(0)
+					esperaCola2.append(tiempoEsperando)
+				except:
+					0
+				retrasoArranque2 = 2
 
 	if(tiempoLiberarFlujo[1]):
 		print("ROJO")
@@ -47,23 +96,3 @@ while tiempoGlobal < 200 :
 			tiempoSemaforo1 = [tiempoSemaforo1Verde, False]
 			tiempoSemaforo2 = [tiempoSemaforo2Verde, True]
 			ciclo = 1
-
-
-
-'''
-if(tiempoGlobal%tiempoSemaforosRojo == 0):
-
-if(contadorCola1%12 == 0):
-	cola1.insert( 0 , 1 )
-
-if (contadorCola2%9 == 0):
-	cola2.insert( 0 , 1 )
-
-if(tiempoSemaforo1Verde==70):
-	tiempoSemaforo1Verde = 0
-	cola1.pop(0) 
-'''
-
-
-
-
